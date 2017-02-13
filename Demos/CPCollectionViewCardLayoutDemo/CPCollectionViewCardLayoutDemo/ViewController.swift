953f29b68024b52fb900031da4fad802616e9738
Parsifal <zmw@izmw.me>
2017-02-13 15:15:18 +0800

    add card demo project.

---
+++ Demos/CPCollectionViewCardLayoutDemo/CPCollectionViewCardLayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPCollectionViewCardLayoutDemo
+//
+//  Created by Parsifal on 2017/2/13.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
