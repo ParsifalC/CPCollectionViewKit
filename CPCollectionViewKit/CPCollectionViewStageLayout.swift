7d8863c2950f165d1e969ba2a58846920164fbd8
Parsifal <zmw@izmw.me>
2017-02-18 20:17:45 +0800

    add leave stage animation.

--- CPCollectionViewKit/CPCollectionViewStageLayout.swift
+++ CPCollectionViewKit/CPCollectionViewStageLayout.swift
@@ -14,11 +14,19 @@ public enum CPMoveAnimationStyle {
     case somefault
 }
 
+public enum CPLeaveStageAnimationStyle {
+    case fadeAway
+    case zoomin
+    case outOfBoundary
+    case fadeAwayAndZoomin
+    case blend
+}
+
 open class CPStageLayoutConfiguration: CPLayoutConfiguration {
     
     public var topCellSize = CGSize(width: 100, height: 100)
-    public var rotateInOut = true
     public var moveAnimationStyle: CPMoveAnimationStyle = .none
+    public var leaveStageAnimationStyle: CPLeaveStageAnimationStyle = .fadeAway
     
 }
 
@@ -64,9 +72,30 @@ open class CPCollectionViewStageLayout: CPCollectionViewLayout {
         attributes.isHidden = false
         
         if itemOffset > -1 && itemOffset<0 {
-            attributes.alpha = fabs(itemOffset)
-            centerX = collectionView.contentOffset.x+(fabs(itemOffset)+0.5)*width
+            if configuration.leaveStageAnimationStyle == .outOfBoundary
+                || configuration.leaveStageAnimationStyle == .blend{
+                centerX = collectionView.contentOffset.x+(fabs(itemOffset)+0.5)*width
+            } else {
+                centerX = collectionView.contentOffset.x+0.5*width
+            }
             centerY = (height-cellHeight)/2
+
+            if configuration.leaveStageAnimationStyle == .fadeAway
+                || configuration.leaveStageAnimationStyle == .fadeAwayAndZoomin
+                || configuration.leaveStageAnimationStyle == .blend {
+                attributes.alpha = (1+itemOffset)
+            } else {
+                attributes.alpha = 1
+            }
+            
+            if configuration.leaveStageAnimationStyle == .zoomin
+                || configuration.leaveStageAnimationStyle == .fadeAwayAndZoomin
+                || configuration.leaveStageAnimationStyle == .blend {
+                cellSize = CGSize(width: (1+itemOffset)*topCellSize.width,
+                                  height: (1+itemOffset)*topCellSize.height)
+            } else {
+                cellSize = topCellSize
+            }
         } else if itemOffset<=1 && itemOffset>=0 {
             centerX = ((width-cellWidth)/2)*(1-itemOffset)+cellWidth/2+collectionView.contentOffset.x
             centerY = ((cellHeight/2-height)/(2))*(1-itemOffset)+height-cellHeight/2
@@ -84,12 +113,12 @@ open class CPCollectionViewStageLayout: CPCollectionViewLayout {
         let rotateFactor = fabs(itemOffset*100).remainder(dividingBy: 100)/100
         
         switch configuration.moveAnimationStyle {
-        case .waltz:
+        case .waltz where itemOffset>0:
             attributes.transform3D =  CATransform3DRotate(attributes.transform3D, CGFloat(M_PI*2)*rotateFactor, 0, 1, 0)
-        case .somefault:
+        case .somefault where itemOffset>0:
             attributes.transform3D =  CATransform3DRotate(attributes.transform3D, CGFloat(M_PI*2)*rotateFactor, 0, 0, 1)
         default:
-            attributes.transform3D = CATransform3D()
+            attributes.transform3D = CATransform3DIdentity
         }
         
 //        print("item:\(item) itemOffset:\(itemOffset) topItemIndex:\(topItemIndex)")
