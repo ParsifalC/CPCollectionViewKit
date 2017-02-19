eca26c65857142ed19a52f8beb737979640921be
Parsifal <zmw@izmw.me>
2017-02-19 11:54:47 +0800

    add initial transition layout

---
+++ CPCollectionViewKit/CPCollectionViewTransitionLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CPCollectionViewTransitionLayout.swift
+//  CPCollectionViewKit
+//
+//  Created by Parsifal on 2017/2/18.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+import UIKit
+
+open class CPCollectionViewTransitionLayout: UICollectionViewTransitionLayout {
+    
+}
---
+++ CPCollectionViewKit/CPCollectionViewTransitionManager.swift
@@ -0,0 +1,9 @@
+//
+//  CPCollectionViewTransitionManager.swift
+//  CPCollectionViewKit
+//
+//  Created by Parsifal on 2017/2/18.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+import Foundation
