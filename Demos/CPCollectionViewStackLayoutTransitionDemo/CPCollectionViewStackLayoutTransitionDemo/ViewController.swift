10e9dddcccc7f785ceae67911fcfc4843b29ba78
Parsifal <zmw@izmw.me>
2017-02-21 19:53:13 +0800

    add initial transition demo.

--- Demos/CPCollectionViewStackLayoutTransitionDemo/CPCollectionViewStackLayoutTransitionDemo/ViewController.swift
+++ Demos/CPCollectionViewStackLayoutTransitionDemo/CPCollectionViewStackLayoutTransitionDemo/ViewController.swift
@@ -7,19 +7,73 @@
 //
 
 import UIKit
+import CPCollectionViewKit
+
+enum LayoutType {
+    case flowLayout
+    case stageLayout
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let cellIdentifier = "CPCollectionViewCell"
+    var dataArray = [(index: Int, color: UIColor)]()
+    var flowLayout: UICollectionViewFlowLayout!
+    var stageLayout: CPCollectionViewStageLayout!
+    var currentLayoutType: LayoutType = .flowLayout
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Data Array
+        for index in 0...29 {
+            dataArray.append((index, randomColor()))
+        }
+        
+        //UI
+        flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.itemSize = CGSize(width: view.bounds.width/3,
+                                     height: view.bounds.width/3)
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        collectionView.setCollectionViewLayout(flowLayout, animated: false)
+        
+        let stageConfiguration = CPStageLayoutConfiguration.init(withCellSize: CGSize(width: 50, height: 50))
+        stageLayout = CPCollectionViewStageLayout(withConfiguration: stageConfiguration)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func randomColor() -> UIColor {
+        return UIColor(red: CGFloat(Double(arc4random_uniform(256))/255.0),
+                       green: CGFloat(Double(arc4random_uniform(256))/255.0),
+                       blue: CGFloat(Double(arc4random_uniform(256))/255.0),
+                       alpha: 1)
     }
-
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.backgroundColor = dataArray[indexPath.item].color
+        
+        return cell
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let layout = currentLayoutType == .flowLayout ? stageLayout : flowLayout
+        collectionView.setCollectionViewLayout(layout, animated: true) { (_) in
+            self.currentLayoutType = self.currentLayoutType == .flowLayout ? .stageLayout : .flowLayout
+        }
+    }
+}
